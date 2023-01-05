d06518c26e32464e1d96cb0963b6877f5c3a102b
doviettung96 <tunga9td@gmail.com>
2023-01-05 04:40:51 +0000

    Refactor

--- GCGL/Baselines/bare_model.py
+++ GCGL/Baselines/bare_model.py
@@ -19,6 +19,7 @@ class NET(torch.nn.Module):
         :param args: The arguments containing the configurations of the experiments including the training parameters like the learning rate, the setting confugurations like class-IL and task-IL, etc. These arguments are initialized in the train.py file and can be specified by the users upon running the code.
 
         """
+
     def __init__(self,
                  model,
                  args):
@@ -27,7 +28,6 @@ class NET(torch.nn.Module):
         # setup network
         self.net = model
         self.optimizer = Adam(model.parameters(), lr=args['lr'])
-        self.data_loader = None
 
     def forward(self, features):
         output = self.net(features)
@@ -43,12 +43,14 @@ class NET(torch.nn.Module):
                 :param args: Same as the args in __init__().
 
                 """
+
         self.net.train()
         train_meter = Meter()
         for batch_id, batch_data in enumerate(data_loader):
             smiles, bg, labels, masks = batch_data
+            bg = bg.to(f"cuda:{args['gpu']}")
             labels, masks = labels.cuda(), masks.cuda()
-            logits = predict(args, self.net, bg.to(f"cuda:{args['gpu']}"))
+            logits = predict(args, self.net, bg)
 
             # Mask non-existing labels
             loss = loss_criterion(logits, labels) * (masks != 0).float()
@@ -63,22 +65,22 @@ class NET(torch.nn.Module):
 
     def observe_tskIL_multicls(self, data_loader, loss_criterion, task_i, args):
         """
-                        The method for learning the given tasks under the task-IL setting with multi-class classification datasets.
+        The method for learning the given tasks under the task-IL setting with multi-class classification datasets.
 
-                        :param data_loader: The data loader for mini-batch training.
-                        :param loss_criterion: The loss function.
-                        :param task_i: Index of the current task.
-                        :param args: Same as the args in __init__().
+        :param data_loader: The data loader for mini-batch training.
+        :param loss_criterion: The loss function.
+        :param task_i: Index of the current task.
+        :param args: Same as the args in __init__().
 
-                        """
+        """
         # task Il under multi-class setting
         self.net.train()
-        #train_meter = Meter()
         clss = args['tasks'][task_i]
         for batch_id, batch_data in enumerate(data_loader[task_i]):
             smiles, bg, labels, masks = batch_data
+            bg = bg.to(f"cuda:{args['gpu']}")
             labels, masks = labels.cuda(), masks.cuda()
-            logits = predict(args, self.net, bg.to(f"cuda:{args['gpu']}"))
+            logits = predict(args, self.net, bg)
 
             # class balance
             n_per_cls = [(labels == j).sum() for j in clss]
@@ -94,22 +96,25 @@ class NET(torch.nn.Module):
 
     def observe_clsIL(self, data_loader, loss_criterion, task_i, args):
         """
-                                The method for learning the given tasks under the class-IL setting with multi-class classification datasets.
+        The method for learning the given tasks under the class-IL setting with multi-class classification datasets.
 
-                                :param data_loader: The data loader for mini-batch training.
-                                :param loss_criterion: The loss function.
-                                :param task_i: Index of the current task.
-                                :param args: Same as the args in __init__().
+        :param data_loader: The data loader for mini-batch training.
+        :param loss_criterion: The loss function.
+        :param task_i: Index of the current task.
+        :param args: Same as the args in __init__().
 
-                                """
+        """
+
         self.net.train()
         clss = []
         for tid in range(task_i + 1):
             clss.extend(args['tasks'][tid])
+
         for batch_id, batch_data in enumerate(data_loader[task_i]):
             smiles, bg, labels, masks = batch_data
+            bg = bg.to(f"cuda:{args['gpu']}")
             labels, masks = labels.cuda(), masks.cuda()
-            logits = predict(args, self.net, bg.to(f"cuda:{args['gpu']}"))
+            logits = predict(args, self.net, bg)
 
             # class balance
             n_per_cls = [(labels == j).sum() for j in clss]
