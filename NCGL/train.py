b34d15d7e762aabf64d6e501224c644884f3cc75
doviettung96 <tunga9td@gmail.com>
2023-01-19 05:34:14 +0000

    Reduce the number of hyperparams tuning

--- NCGL/train.py
+++ NCGL/train.py
@@ -44,15 +44,15 @@ if __name__ == '__main__':
                                  'attn_drop': .6, 'negative_slope': 0.2, 'residual': False})
     parser.add_argument('--GCN-args', default={'h_dims': [256], 'dropout': 0.0, 'batch_norm': False})
     parser.add_argument('--GIN-args', default={'h_dims': [256], 'dropout': 0.0})
-    parser.add_argument('--ergnn_args', type=str2dict, default={'budget': [10, 100, 1000], 'd': [0.05, 0.5, 5.0], 'sampler': ['CM, MF', 'random']},
+    parser.add_argument('--ergnn_args', type=str2dict, default={'budget': [10, 100, 1000], 'd': [0.5], 'sampler': ['CM']},
                         help='sampler options: CM, CM_plus, MF, MF_plus')
-    parser.add_argument('--erreplace_args', type=str2dict, default={'budget': [20, 200, 2000], 'd': [0.05, 0.5, 5.0], 'sampler': ['CM', 'MF', 'random'], 'max_size': [0.5]},
+    parser.add_argument('--erreplace_args', type=str2dict, default={'budget': [20, 200, 2000], 'd': [0.5], 'sampler': ['CM'], 'max_size': [0.5]},
                         help='sampler options: CM, CM_plus, MF, MF_plus')
-    parser.add_argument('--dce_args', type=str2dict, default={'budget': [20, 200, 2000], 'd': [0.05, 0.5, 5.0], 'sampler': ['CM', 'MF', 'random'], 'max_size': [0.5]},
+    parser.add_argument('--dce_args', type=str2dict, default={'budget': [20, 200, 2000], 'd': [0.5], 'sampler': ['CM'], 'max_size': [0.5]},
                         help='sampler options: CM, CM_plus, MF, MF_plus')
-    parser.add_argument('--sl_args', type=str2dict, default={'budget': [20, 200, 2000], 'd': [0.05, 0.5, 5.0], 'sampler': ['CM', 'MF', 'random'], 'max_size': [0.5]},
+    parser.add_argument('--sl_args', type=str2dict, default={'budget': [20, 200, 2000], 'd': [0.5], 'sampler': ['CM'], 'max_size': [0.5]},
                         help='sampler options: CM, CM_plus, MF, MF_plus')
-    parser.add_argument('--our_args', type=str2dict, default={'budget': [20, 200, 2000], 'd': [0.05, 0.5, 5.0], 'sampler': ['CM', 'MF', 'random'], 'max_size': [0.5]},
+    parser.add_argument('--our_args', type=str2dict, default={'budget': [20, 200, 2000], 'd': [0.5], 'sampler': ['CM'], 'max_size': [0.5]},
                         help='sampler options: CM, CM_plus, MF, MF_plus')
     parser.add_argument('--lwf_args', type=str2dict, default={'lambda_dist': [1.0, 10.0], 'T': [2.0, 20.0]})
     parser.add_argument('--twp_args', type=str2dict, default={'lambda_l': 10000., 'lambda_t': 10000., 'beta': 0.01})
